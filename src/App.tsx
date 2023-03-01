f037de2de1795c3551c4ba5d32d9a43860d1adf8
fry <fry.land@hotmail.fr>
2023-03-01 09:42:23 +0100

    fetching todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
-import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Todos from './pages/Todos';
--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,3 +1,44 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export interface Todo {
+  id: string;
+  description: string;
+  status: boolean;
+}
+
+type TodoList = Todo[];
+
 export default function Todos() {
-  return <div>Todos</div>;
+  const [todos, setTodos] = useState<TodoList | undefined>();
+
+  const token = JSON.parse(localStorage.getItem('user') || '');
+  console.log(token.access_token);
+
+  async function getTodos() {
+    const response = await axios.get<TodoList>('http://localhost:5500/todos', {
+      headers: {
+        Authorization: `Bearer ${token.access_token}`,
+      },
+    });
+
+    setTodos(response.data);
+    console.log(response.data);
+
+    return response.data;
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <div>
+      {todos?.length === 0 ? (
+        <p>There is no todos</p>
+      ) : (
+        todos?.map((todo) => <li key={todo.id}>{todo.description}</li>)
+      )}
+    </div>
+  );
 }
